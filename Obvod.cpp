7fb0ad7a66595349eede3f108303d113ca88f089
Zbyněk <Anub.W@seznam.cz>
2015-04-05 17:06:09 +0200

    ! Unsupported commit message encoding: fatal: ambiguous argument 'HEAD': unknown revision or path not in the working tree. !
    Use '--' to separate paths from revisions, like this:
    'git <command> [<revision>...] -- [<file>...]'

---
+++ Obvod.cpp
@@ -0,0 +1,11 @@
+#include "std_lib_facilities.h"
+using namespace std;
+int main()
+{
+	
+	cout << "Hello\n";
+	cout << "all   \n\t";
+	cout << "std_lib_facilities.h is ready" << endl;
+	keep_window_open();
+	return 0;
+}
